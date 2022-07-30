d68eff5adc6233826526a85a187f912b8e7030a2
Alfart18 <019200997j@uandina.edu.pe>
2022-07-30 12:26:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './DS.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div class="App">
+      <div class="App-header">
+        <div class="container">
+          <div class="row">
+            <div class="col-2">
+              <img src={logo} alt="Logo_DS"></img>
+            </div>
+            <div class="col-8">
+              <h1>Hadware y Software</h1>
+            </div>
+            <div class="img_mov">
+              <center><marquee behavior="alternate" direction="right" width="170%"> <img src="https://www.lennu.net/wp-content/uploads/2015/01/windows-mac-linux.png" /> </marquee></center>
+            </div> 
+              <div class="container">
+                <div class="row">
+                  <div class="col order-last">
+                    1
+                  </div>
+                  <div class="col">
+                    2
+                  </div>
+                  <div class="col order-first">
+                    3
+                  </div>
+                </div>     
+                <div class="row">
+                  <div class="col order-last">
+                    4
+                  </div>
+                  <div class="col">
+                    5
+                  </div>
+                  <div class="col order-first">
+                    6
+                  </div>
+                </div>   
+                <div class="row">
+                  <div class="col order-last">
+                    7
+                  </div>
+                  <div class="col">
+                    8
+                  </div>
+                  <div class="col order-first">
+                    9
+                  </div>
+                </div>   
+                <div class="row">
+                  <div class="col order-last">
+                    10
+                  </div>
+                  <div class="col">
+                    11
+                  </div>
+                  <div class="col order-first">
+                    12
+                  </div>
+                </div>    
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>);
 }
 
 export default App;
