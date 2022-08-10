7f4ae6e601127d9137f3f69e6482683c86b56e5f
Alfart18 <019200997j@uandina.edu.pe>
2022-08-10 12:33:06 -0500

    Nuevos Cambios

--- src/App.js
+++ src/App.js
@@ -1,71 +1,152 @@
 import './App.css';
-import logo from './DS.png'
+import logo_DS from './DS.png'
+import logo_Linux from './Linux.png'
+import logo_HP from './HP.png'
+import logo_AMD from './AMD.png'
+import logo_Asus from './Asus.png'
+import logo_Logitech from './Logitech.png'
+import logo_Windows from './Windows.png'
 
 function App() {
   return (
-    <div class="App">
-      <div class="App-header">
-        <div class="container">
-          <div class="row">
-            <div class="col-2">
-              <img src={logo} alt="Logo_DS"></img>
-            </div>
-            <div class="col-8">
-              <h1>Hadware y Software</h1>
-            </div>
-            <div class="img_mov">
-              <center><marquee behavior="alternate" direction="right" width="170%"> <img src="https://www.lennu.net/wp-content/uploads/2015/01/windows-mac-linux.png" /> </marquee></center>
-            </div> 
-              <div class="container">
-                <div class="row">
-                  <div class="col order-last">
-                    1
-                  </div>
-                  <div class="col">
-                    2
-                  </div>
-                  <div class="col order-first">
-                    3
-                  </div>
-                </div>     
-                <div class="row">
-                  <div class="col order-last">
-                    4
-                  </div>
-                  <div class="col">
-                    5
-                  </div>
-                  <div class="col order-first">
-                    6
-                  </div>
-                </div>   
-                <div class="row">
-                  <div class="col order-last">
-                    7
-                  </div>
-                  <div class="col">
-                    8
-                  </div>
-                  <div class="col order-first">
-                    9
-                  </div>
-                </div>   
-                <div class="row">
-                  <div class="col order-last">
-                    10
-                  </div>
-                  <div class="col">
-                    11
-                  </div>
-                  <div class="col order-first">
-                    12
-                  </div>
-                </div>    
-            </div>
-          </div>
-        </div>
+  
+    <div class="container p-4 my-4 bg-light text-warning">
+      <div class="row style="text-align: center;">
+      <div class="col-sm-3" style="text-align: center;">
+        <img src={logo_DS} alt="Logo_DS"></img>
       </div>
-    </div>);
+      <div class="col-sm-9" style="text-align: center;">
+        <img src={logo_Linux} alt="Logo_Linux" style="width:100px;height:80px;"></img>
+        <img src={logo_HP} alt="Logo_HP" style="width:100px;height:80px;"></img>
+        <img src={logo_AMD} alt="Logo_AMD" style="width:100px;height:80px;"></img>
+        <img src={logo_Asus} alt="Logo_Asus" style="width:100px;height:80px;"></img>
+        <img src={logo_Logitech} alt="Logo_Logitech" style="width:90px;height:80px;"></img>
+        <img src={logo_Windows} alt="Logo_Windows" style="width:80px;height:80px;"></img>
+
+      </div>
+    </div>
+</div>
+<div class="container p-4 my-4 bg-dark text-warning">
+    <div class="row" style="text-align: center;">
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/placa">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="fal fa-server"></i></div>
+                        <div class="icontitle">Placa</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/procesador">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="fal fa-microchip"></i></div>
+                        <div class="icontitle">CPU</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/RAM">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="fal fa-memory"></i></div>
+                        <div class="icontitle">RAM</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/placa">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="fal fa-desktop"></i></div>
+                        <div class="icontitle">Graficos</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+    </div>
+
+
+    <div class="row" style="text-align: center;">
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/sistema">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="bi bi-windows"></i></i></div>
+                        <div class="icontitle">Sistema</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/placa">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="fal fa-chart-line"></i></div>
+                        <div class="icontitle">Procesos</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/disco">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="bi bi-hdd-fill"></i></div>
+                        <div class="icontitle">Disco Duro</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/USB">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="bi bi-usb-symbol"></i></div>
+                        <div class="icontitle">USB</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+    </div>
+
+    <div class="row" style="text-align: center;">
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/bios">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="fal fa-th-list"></i></div>
+                        <div class="icontitle">Bios</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/red">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="bi bi-diagram-3-fill"></i></div>
+                        <div class="icontitle">Red</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/placa">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="bi bi-wifi"></i></div>
+                        <div class="icontitle">Wifi</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+        <div class="col-sm-3"><a href="http://localhost:3000/DS/programas">
+                <button type="button" class="btn btn-warning" style="width:100px;height:80px; margin-bottom: 5%;">
+                    <div class="inner">
+                        <div class="icons"><i class="bi bi-github"></i></div>
+                        <div class="icontitle">Programas</div>
+                    </div>
+                </button>
+            </a>
+        </div>
+    </div>
+</div >);
 }
+
 
 export default App;
